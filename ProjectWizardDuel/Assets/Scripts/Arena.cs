12929a79c96e9e92d1e57ceb7e82bf8c6f36d09d
Mohamed <mhmd.tarek94@gmail.com>
2016-08-15 15:58:02 -0400

    Added Arena scene + auto-generation of arena floor

---
+++ ProjectWizardDuel/Assets/Scripts/Arena.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arena : MonoBehaviour {
+
+	public FloorCell floorCellPrefab;
+	public IntVector2 size;
+
+	FloorCell[,] floorCells;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void Generate () {
+		floorCells = new FloorCell[size.x, size.z];
+		IntVector2 coordinates;
+		coordinates.x = 0;
+		coordinates.z = 0;
+
+		while (coordinates.x < size.x) {
+			while (coordinates.z < size.z) {
+				CreateCell(coordinates);
+				coordinates.z++;
+			}
+			coordinates.z = 0;
+			coordinates.x++;
+		}
+	}
+
+
+	FloorCell CreateCell (IntVector2 coordinates) {
+		FloorCell newCell = Instantiate(floorCellPrefab) as FloorCell;
+		newCell.name = "Arena Floor Cell " + coordinates.x + ", " + coordinates.z;
+
+		floorCells[coordinates.x, coordinates.z] = newCell;
+		newCell.coordinates = coordinates;
+
+		newCell.transform.parent = transform;
+		newCell.transform.localPosition = new Vector3(coordinates.x - size.x * 0.5f + 0.5f, 0f, coordinates.z - size.z * 0.5f + 0.5f);
+
+		return newCell;
+	}
+}
---
+++ ProjectWizardDuel/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public Arena arenaPrefab;
+
+	Arena arenaInstance;
+
+
+	// Use this for initialization
+	void Start () {
+		BeginGame();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void BeginGame () {
+		arenaInstance = Instantiate(arenaPrefab) as Arena;
+		arenaInstance.Generate();
+	}
+}
