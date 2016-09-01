b73cc4d6085bfc040101e46e8a95f7d10e50bc45
Brandon Mubarak <bdmubara@uwaterloo.ca>
2016-09-01 16:01:40 -0400

    Merged HUD with new player controls

---
+++ ProjectWizardDuel/Assets/Scripts/Player/PlayerStatController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// IMPORTANT NOTE: Temporary class until functionality can be distributed amongst proper scripts
+public class PlayerStatController : MonoBehaviour {
+
+	[SerializeField]
+	private Stat shortMana;
+	[SerializeField]
+	private Stat longMana;
+	[SerializeField]
+	private Stat health;
+
+	private void Awake () {
+		shortMana.Initialize();
+		longMana.Initialize();
+		health.Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			shortMana.CurVal -= 10;
+		}
+	}
+}
---
+++ ProjectWizardDuel/Assets/Scripts/Player/Stat.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[Serializable]
+public class Stat {
+	[SerializeField]
+	private BarScript bar;
+	[SerializeField]
+	private float maxVal;
+	[SerializeField]
+	private float curVal;
+
+	public float CurVal {
+		get {
+			return curVal;
+		}
+		set {
+			this.curVal = value;
+			bar.Value = curVal;
+		}
+	}
+
+	public float MaxVal {
+		get {
+			return maxVal;
+		}
+		set {
+			this.maxVal = value;
+			bar.MaxValue = maxVal;
+		}
+	}
+
+	public void Initialize() {
+		this.MaxVal = maxVal;
+		this.CurVal = curVal;
+	}
+}
